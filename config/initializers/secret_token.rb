431a1ac00189b5a6a1e900513fdcb90a0ce4fcf6
root <yacchin0101@gmail.com>
2014-06-22 15:13:10 +0900

    change secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,10 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-TodoApi::Application.config.secret_key_base = 'b74696cf3ee50ddb30eb1719b3f98683b2eaaefa02cf713f426273fb3d1172442ffb095e363e97ab8db358eb52142e3a2e26bf74f6becab29eccae76e157c90d'
+#TodoApi::Application.config.secret_key_base = 'b74696cf3ee50ddb30eb1719b3f98683b2eaaefa02cf713f426273fb3d1172442ffb095e363e97ab8db358eb52142e3a2e26bf74f6becab29eccae76e157c90d'
+
+def secure_token
+  ((0..9).to_a + ("a".."z").to_a + ("A".."Z").to_a).sample(50).join
+end
+
+TodoApi::Application.config.secret_key_base = secure_token
