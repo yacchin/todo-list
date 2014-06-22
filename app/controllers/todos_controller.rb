3dc0473732d2e6c4060a8be1b6a252f911b46f69
root <yacchin0101@gmail.com>
2014-06-22 15:50:39 +0900

    delete files.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -12,60 +12,6 @@ class TodosController < ApplicationController
     gon.api_url = Settings.api.url
   end
 
-  # GET /todos/1
-  # GET /todos/1.json
-  def show
-  end
-
-  # GET /todos/new
-  def new
-    @todo = Todo.new
-  end
-
-  # GET /todos/1/edit
-  def edit
-  end
-
-  # POST /todos
-  # POST /todos.json
-  def create
-    @todo = Todo.new(todo_params)
-
-    respond_to do |format|
-      if @todo.save
-        format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @todo }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /todos/1
-  # PATCH/PUT /todos/1.json
-  def update
-    respond_to do |format|
-      if @todo.update(todo_params)
-        format.html { redirect_to @todo, notice: 'Todo was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /todos/1
-  # DELETE /todos/1.json
-  def destroy
-    @todo.destroy
-    respond_to do |format|
-      format.html { redirect_to todos_url }
-      format.json { head :no_content }
-    end
-  end
-
   def get_todo
     user = get_user(params[:api_key])
     update_todos(params[:todos],user) if params.has_key?(:todos)
